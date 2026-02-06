b79f8a08ccfe2700817debe0fabd8bd51047d87b
mttw-frd <matthew.ferdib@gmail.com>
2026-02-07 03:12:10 +0800

    capitalize account titles

--- main.gd
+++ main.gd
@@ -4,30 +4,30 @@ var entry = preload("res://entry.tscn")
 var account_titles = []
 
 func _ready() -> void:
-	$AddEntry.pressed.connect(add_entry)
-	$Collect.pressed.connect(collect_info)
+	$Journal/AddEntry.pressed.connect(add_entry)
+	$Journal/Collect.pressed.connect(collect_info)
 
 func add_entry():
 	var new_entry = entry.instantiate()
-	$JournalScroll/VBoxContainer.add_child(new_entry)
+	$Journal/JournalScroll/VBoxContainer.add_child(new_entry)
 
 func to_ledger(entries):
 	for ent in entries:
 		#print("TEST: ", ent.debit_title)
-		if !account_titles.has(ent.debit_title):
-			account_titles.append(ent.debit_title)
+		if !account_titles.has(ent.debit_title.to_upper()):
+			account_titles.append(ent.debit_title.to_upper())
 		elif ent.debit_title == "":
 			pass
 			
-		if !account_titles.has(ent.credit_title):
-			account_titles.append(ent.credit_title)
-		elif ent.debit_title == "":
+		if !account_titles.has(ent.credit_title.to_upper()):
+			account_titles.append(ent.credit_title.to_upper())
+		elif ent.credit_title == "":
 			pass
 			
 	print(account_titles)
 
 func collect_info():
-	var entries = $JournalScroll/VBoxContainer.get_children()
+	var entries = $Journal/JournalScroll/VBoxContainer.get_children()
 	var all_entries = []
 	for current_entry in entries:
 		#print(current_entry.request_information())
