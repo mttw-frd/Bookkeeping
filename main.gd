b9fc4e80f3c1c777d13a575cf01f045804590d5b
mttw-frd <matthew.ferdib@gmail.com>
2026-02-07 02:54:50 +0800

    start of ledger part

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var entry = preload("res://entry.tscn")
+var account_titles = []
 
 func _ready() -> void:
 	$AddEntry.pressed.connect(add_entry)
@@ -12,7 +13,18 @@ func add_entry():
 
 func to_ledger(entries):
 	for ent in entries:
-		print(ent.debit_title)
+		#print("TEST: ", ent.debit_title)
+		if !account_titles.has(ent.debit_title):
+			account_titles.append(ent.debit_title)
+		elif ent.debit_title == "":
+			pass
+			
+		if !account_titles.has(ent.credit_title):
+			account_titles.append(ent.credit_title)
+		elif ent.debit_title == "":
+			pass
+			
+	print(account_titles)
 
 func collect_info():
 	var entries = $JournalScroll/VBoxContainer.get_children()
@@ -20,6 +32,6 @@ func collect_info():
 	for current_entry in entries:
 		#print(current_entry.request_information())
 		all_entries.append(current_entry.request_information())
-	print(all_entries)
+	#print(all_entries)
 	to_ledger(all_entries)
 	return all_entries
